800666082af103bd7b3dfc27b7f48aa28a7eb89c
Big Jump USA <88063656+bigjumpusa@users.noreply.github.com>
2025-03-30 20:09:31 -0300

    Create database.py

---
+++ app/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@localhost:5432/tasks")
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def get_db():
+    async with async_session() as session:
+        yield session
